8486ee8845351f677b673c72a85f3a26f8960683
mehedihasanit242 <mehedihasanit242@gmail.com>
2023-03-29 20:18:39 +0600

    change1

---
+++ project.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    printf("enter your first number:");
+    scanf("%d", &num1);
+    printf("enter your second number:");
+    scanf("%d", &num2);
+    printf("%d + %d =%d\n ", num1, num2, num1 + num2);
+    printf("%d - %d =%d\n ", num1, num2, num1 - num2);
+    printf("%d * %d =%d\n ", num1, num2, num1 * num2);
+    printf("%d / %d =%d\n ", num1, num2, num1 / num2);
+    return 0;
+}
