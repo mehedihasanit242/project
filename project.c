7c1f611db4010ad5714591e6d26dbf0e1b5599b7
mehedihasanit242 <mehedihasanit242@gmail.com>
2023-03-31 16:35:05 +0600

    computer programming 41 pages

--- project.c
+++ project.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int num1, num2;
+    int num1, num2,sum;
+    char a = '+';
     printf("enter your first number:");
     scanf("%d", &num1);
     printf("enter your second number:");
     scanf("%d", &num2);
-    printf("%d + %d =%d\n ", num1, num2, num1 + num2);
-    printf("%d - %d =%d\n ", num1, num2, num1 - num2);
-    printf("%d * %d =%d\n ", num1, num2, num1 * num2);
-    printf("%d / %d =%d\n ", num1, num2, num1 / num2);
-    printf("the end");
+    sum = num1 + num2;
+    printf("%d %c %d =%d\n ", num1,a, num2, sum);
+    
     return 0;
 }
