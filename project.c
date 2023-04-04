be1b3de79f90a250646edb73c33b0ddb25fddaa2
mehedihasanit242 <mehedihasanit242@gmail.com>
2023-04-04 15:03:51 +0600

    leap year 1

--- project.c
+++ project.c
@@ -4,12 +4,12 @@ int main()
     int n;
     printf("enter your year:");
     scanf("%d", &n);
-    if (n%4==0&&n%100!=0)
+    if (n%4==0 && n%100!=0)
     {
         printf("%d is leap year", n);
 
     }
-    else if (n%4==0&&n%100==0&&n%400==0)
+    else if (n%4==0 && n%100==0 && n%400==0)
     {
         /* code */
         printf(" %d is leap year ", n);
